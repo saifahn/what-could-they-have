1e53a555534333a284269b7ddeda355f7c4e0dee
Sean Li <li.saifahn@gmail.com>
2019-03-10 21:45:38 +0100

    Generate mana with length and number of colours

--- src/functions/generateMana.test.ts
+++ src/functions/generateMana.test.ts
@@ -1,7 +1,56 @@
 import generateMana from './generateMana'
 
+/* 
+
+TODO:
+
+- [ ] Accept a set object
+- [ ] Longest possible of one colour
+- [ ] Generate multiple colours
+- [ ] Has the possibility to include the most expensive spell
+- [ ] Accept a length argument, doesn't make it longer than that
+
+*/
+
 it('generates one colour of mana', () => {
-  // function generates one colour of mana
-  const mana: string = generateMana()
+  const mana = generateMana({ len: 4, coloursToGenerate: 1 })
   expect(mana).toBeTruthy()
 })
+
+it('generates a random string of mana', () => {
+  const testParams = { len: 4, coloursToGenerate: 4 }
+  const mana = generateMana(testParams)
+  const manaTwo = generateMana(testParams)
+  expect(mana).not.toEqual(manaTwo)
+})
+
+function getNumberOfColours(manaString: string) {
+  // loop through string
+  // get number of unique characters
+  // TODO: get unique from string
+  const uniqueColours: string[] = []
+  manaString.split('').forEach((character) => {
+    if (uniqueColours.indexOf(character) === -1) {
+      uniqueColours.push(character)
+    }
+  })
+  return uniqueColours.length
+}
+
+it('can generate two colours of mana', () => {
+  // TODO: rename variables
+  const len = 4
+  const coloursToGenerate = 2
+  const testParams = {
+    len,
+    coloursToGenerate
+  }
+  const mana = generateMana(testParams)
+  console.log(mana)
+  const numGeneratedColours = getNumberOfColours(mana)
+  expect(numGeneratedColours).toBeLessThanOrEqual(coloursToGenerate)
+})
+
+// sometimes it doesn't generate two different colours when 2 is passed in
+// right now it only works for up to the amount of characters
+// that might actually be fine
--- src/functions/generateMana.ts
+++ src/functions/generateMana.ts
@@ -1,6 +1,33 @@
-function generateMana(): string {
-  //
-  return 'W'
+const COLOURS = ['W', 'U', 'B', 'R', 'G']
+
+function getColours(numColours: number): string[] {
+  // make a copy of the old array
+  const initColours = COLOURS.slice()
+  const colours: string[] = []
+  for (let i = 0; i < numColours; i++) {
+    // remove a random colour each time
+    const length = initColours.length
+    const randomNum = Math.floor(Math.random() * length)
+    colours.push(initColours.splice(randomNum, randomNum + 1)[0])
+  }
+  return colours
+}
+
+interface generateManaParams {
+  len: number
+  coloursToGenerate: number
+}
+
+function generateMana({ len, coloursToGenerate }: generateManaParams): string {
+  const colours = getColours(coloursToGenerate)
+  let mana = ''
+  for (let i = 0; i < len; i++) {
+    // add a random number of different colours from the array
+    const coloursLength = colours.length
+    const randomNum = Math.floor(Math.random() * coloursLength)
+    mana += colours[randomNum]
+  }
+  return mana
 }
 
 export default generateMana
