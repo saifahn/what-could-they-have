8032c15d0b7143df5c6f49fb7bfe7f024036fd15
Sean Li <li.saifahn@gmail.com>
2019-03-13 23:57:53 +0100

    Refactor to use a mana object

--- src/functions/canBeCast.test.ts
+++ src/functions/canBeCast.test.ts
@@ -1,23 +1,135 @@
 import canBeCast, { formatMana } from './canBeCast'
 import { Card } from '../common/types'
 
-const testCards = [
+const testCards: Card[] = [
   {
     name: 'Absorb',
     mana_cost: '{W}{U}{U}',
     cmc: 3,
     type_line: 'Instant'
+  },
+  {
+    name: 'Applied Biomancy',
+    mana_cost: '{G}{U}',
+    cmc: 2,
+    type_line: 'Instant'
+  },
+  {
+    name: 'Angel of Grace',
+    mana_cost: '{3}{W}{W}',
+    cmc: 5,
+    type_line: 'Creature — Angel'
+  },
+  {
+    name: 'Collision // Colossus',
+    mana_cost: '1{R/G} // {R}{G}',
+    cmc: 4,
+    type_line: 'Instant // Instant',
+    layout: 'split',
+    card_faces: [
+      {
+        name: 'Collision',
+        mana_cost: '1{R/G}',
+        type_line: 'Instant'
+      },
+      {
+        name: 'Colossus',
+        mana_cost: '{R}{G}',
+        type_line: 'Instant'
+      }
+    ]
+  },
+  {
+    name: 'Incubation // Incongruity',
+    mana_cost: '{G/U} // {1}{G}{U}',
+    cmc: 4,
+    type_line: 'Sorcery // Instant',
+    layout: 'split',
+    card_faces: [
+      {
+        name: 'Incubation',
+        mana_cost: '{G/U}',
+        type_line: 'Sorcery'
+      },
+      {
+        name: 'Incongruity',
+        mana_cost: '{1}{G}{U}',
+        type_line: 'Instant'
+      }
+    ]
+  },
+  {
+    name: "Lawmage's Binding",
+    mana_cost: '{1}{W}{U}',
+    cmc: 3,
+    type_line: 'Enchantment — Aura'
+  },
+  {
+    name: 'Warrant // Warden',
+    mana_cost: '{W/U}{W/U} // {3}{W}{U}',
+    cmc: 7,
+    type_line: 'Instant // Sorcery',
+    layout: 'split',
+    card_faces: [
+      {
+        name: 'Warrant',
+        mana_cost: '{W/U}{W/U}',
+        type_line: 'Instant'
+      },
+      {
+        name: 'Warden',
+        mana_cost: '{3}{W}{U}',
+        type_line: 'Sorcery'
+      }
+    ]
   }
 ]
 
-test('returns false with uncastable card', () => {
-  const testCost = formatMana('RRGG')
+describe('formatMana function returns a usable manaObject', () => {
+  it('returns an appropriate object based on the cost', () => {
+    const cost = '{2}{W}{U}'
+    const expected = {
+      generic: 2,
+      W: 1,
+      U: 1,
+      cmc: 4
+    }
+    const actual = formatMana(cost)
+    expect(actual).toMatchObject(expected)
+  })
+})
+
+test('returns false with uncastable card by cmc', () => {
+  const testCost = '{R}{G}'
   const result = canBeCast(testCards[0], testCost)
   expect(result).toEqual(false)
 })
 
-test('returns true with castable card', () => {
-  const testCost = formatMana('UUW')
+test('returns false with uncastable card by colour', () => {
+  const testCost = '{R}{R}{W}{W}'
+  const result = canBeCast(testCards[0], testCost)
+  expect(result).toEqual(false)
+})
+
+test('returns true with exactly castable card', () => {
+  const testCost = '{U}{U}{W}'
   const result = canBeCast(testCards[0], testCost)
   expect(result).toEqual(true)
 })
+
+test('returns true with non-exact castable card', () => {
+  const testCost = '{6}{U}{U}{U}{W}'
+  const result = canBeCast(testCards[0], testCost)
+  expect(result).toEqual(true)
+})
+
+// test('returns true with castable split card', () => {})
+
+// test('returns true with castable hybrid card', () => {})
+
+// test('calculates with high generic mana', () => {
+//   // works with a hypothetical spell of {10}{R}{W}
+// })
+
+// test('works with mana in the wrong order')
+// maybe it's fine to assume that the number will always come first?
--- src/functions/canBeCast.ts
+++ src/functions/canBeCast.ts
@@ -1,11 +1,25 @@
 import { Card } from '../common/types'
 
-export function formatMana(cost: string): string {
-  return cost
-    .replace(/[{}]/g, '')
-    .split('')
-    .sort()
-    .join('')
+export function formatMana(cost: string) {
+  const manaRE = /\{([\dWUBRG/]+)\}/g
+  const manaObject = { cmc: 0 }
+  const manaArray = cost.match(manaRE) || []
+  manaArray.forEach((symbol) => {
+    const newSymbol = symbol.replace(manaRE, '$1')
+    if (isNaN(Number(newSymbol))) {
+      if (manaObject.hasOwnProperty(newSymbol)) {
+        manaObject[newSymbol] += 1
+      } else {
+        manaObject[newSymbol] = 1
+      }
+      manaObject['cmc'] += 1
+    } else {
+      const generic = Number(newSymbol)
+      manaObject['generic'] = generic
+      manaObject['cmc'] += generic
+    }
+  })
+  return manaObject
 }
 
 function canBeCast(card: Card, cost: string): boolean {
@@ -13,15 +27,31 @@ function canBeCast(card: Card, cost: string): boolean {
     takes a card and a mana cost
     returns true if the card can be cast
   */
-  // take the card's mana cost
-  // format it
-  // take the cost
-  // format it
-  // compare the two
   if (!card.mana_cost) {
     return false
   }
-  return formatMana(card.mana_cost) == cost
+  const availableMana = formatMana(cost)
+  const cardMana = formatMana(card.mana_cost)
+  // get the cmc of the card from the mana cost
+  // get the cmc of the available mana
+  // return false if cmc is too much
+  if (availableMana.cmc < cardMana.cmc) {
+    return false
+  }
+  // look at coloured symbols
+  const cardKeys = Object.keys(cardMana)
+  const availableKeys = Object.keys(availableMana)
+  // count each one from the costs
+  return cardKeys.every(
+    (key) =>
+      availableKeys.indexOf(key) > -1 && availableMana[key] >= cardMana[key]
+  )
+  // return false if too much of one kind
+  // look at hybrid symbols
+  // return false if not able
+  // return true
+  // return formatMana(card.mana_cost) == cost
+  // return true
 }
 
 export default canBeCast
