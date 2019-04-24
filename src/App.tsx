9ddb2d0c4b1e1fad4fe2113f888bb3806fab0b6f
Sean Li <li.saifahn@gmail.com>
2019-04-24 14:16:21 +0200

    Slight changes to padding on intro

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import Header from './components/Header'
 function App() {
   return (
     <Router>
-      <div className="inner-container py-4">
+      <div className="inner-container pt-4 pb-16">
         <Header />
 
         <Switch>
