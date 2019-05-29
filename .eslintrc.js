92fbb0a3d29a1e7bad7cdcadc2e6aabf52b5b944
Sean Li <li.saifahn@gmail.com>
2019-05-29 22:37:29 +0200

    Add typescript-eslint to the project

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  env: {
+    browser: true,
+    jest: true,
+  },
+}
